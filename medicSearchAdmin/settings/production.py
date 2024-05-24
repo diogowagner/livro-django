d415d01d2c8fd7d7eb8f0fd86a628fb28aecf4ca
diogowagner <diogowagnerms@gmail.com>
2024-05-24 10:20:39 -0300

    Initial

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,22 @@
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = '@w232!43*fnhIoQTCkIp=>kbvRPiKkjro20874@n!jfV49@9'
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Nome do banco de dados',
+        'USER': 'Nome do usuário',
+        'PASSWORD': 'Senha',
+        'HOST': 'Nome do host de conexão ao banco',
+        'PORT': 'Número da porta de comunicação',
+    }
+}
---
+++ medicSearchAdmin/settings/testing.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+DEBUG = True
+
+SECRET_KEY = '@Kkjro2kIp=>kbvRPiV490874@n!jfw232!43*fnhIoQTC@9'
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
